f6a6f71d5f7434265e1248769520bb4094bf9757
deoladed <leo.robert.mk@gmail.com>
2019-02-05 18:18:26 +0100

    Refonte routes a venir

--- app/controllers/potins_controller.rb
+++ app/controllers/potins_controller.rb
@@ -1,9 +1,13 @@
 class PotinsController < ApplicationController
   def show
+    puts '#' *30
+    p params
+    puts '#' *30
   	@potins = Potin.all
-    unless params[:user].nil? && params[:comment].nil?
-      Comment.create(user: User.all.last, content:params[:comment], commenteable: Potin.find(params[:id])) 
-    end
+    Comment.create(user: User.all.last, content:params[:comment], commenteable: Potin.find(params[:id])) unless params[:user].nil? && params[:comment].nil?
+  
+    Like.create(user: User.all.last, likeable: Potin.find(params[:id])) if params[:commit] == 'Liker'
+
   end
 
   def user
